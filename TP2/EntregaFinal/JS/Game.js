cee55e04d8f44ed4fe0e2d4650e776680cbde929
kevinVigil <kevinvigil27@gmail.com>
2023-10-09 21:36:36 -0300

    termine quedo un bug en el game que no pude slucionar, pero bueno, estoy cansado

--- TP2/EntregaFinal/JS/Game.js
+++ TP2/EntregaFinal/JS/Game.js
@@ -1,12 +1,34 @@
+window.onload
+
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 
+const envarAmigos= document.querySelector("#envarAmigos");
+const compartir= document.querySelector("#compartir");
+
+const irAPagar= document.querySelector("#irAPagar");
+const iconoCarrito= document.querySelector("#iconoCarrito");
+
+envarAmigos.addEventListener("click", () => {
+    compartir.classList.add("visible");
+});
+
 abrir.addEventListener("click", () => {
-    event.stopPropagation();
     nav.classList.add("visible");
 
 });
 
+iconoCarrito.addEventListener('click', () => {
+    event.stopPropagation();
+    if (irAPagar.classList.contains("visible")) {
+        irAPagar.classList.remove("visible");
+    } else {
+        irAPagar.classList.add("visible");
+    }
+});
+
 window.onclick = function name(e) {
     nav.classList.remove("visible");
+    irAPagar.classList.remove("visible");
+    compartir.classList.remove("visible");
 }
