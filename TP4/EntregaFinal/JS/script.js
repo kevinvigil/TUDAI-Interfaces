0f772b3f8d99b8145ff2d622210011305bacb7ab
kevinVigil <kevinvigil27@gmail.com>
2023-11-26 17:23:35 -0300

    ya esta todo acomodaddo, hay que empezar los puntos importantes

--- TP4/EntregaFinal/JS/script.js
+++ TP4/EntregaFinal/JS/script.js
@@ -11,3 +11,34 @@ document.addEventListener("scroll", () => {
     logo.classList.remove("visible");
   }
 });
+
+document.addEventListener("scroll", () => {
+  function clean() {
+    document.querySelectorAll(".imgS6").forEach((s) => {
+      s.classList.remove("showImgS6");
+    });
+    document.querySelectorAll(".textosS6").forEach((s) => {
+      s.classList.remove("showTextoS6");
+    });
+  }
+  if (window.scrollY < 4080) {
+    clean();
+    document.querySelector("#imgS6-1").classList.add("showImgS6");
+    document.querySelector("#textS6-1").classList.add("showTextoS6");
+  }
+  if (window.scrollY > 4080 && window.screenY < 4450) {
+    clean();
+    document.querySelector("#imgS6-2").classList.add("showImgS6");
+    document.querySelector("#textS6-2").classList.add("showTextoS6");
+  }
+  if (window.scrollY > 4450 && window.screenY < 4950) {
+    clean();
+    document.querySelector("#imgS6-3").classList.add("showImgS6");
+    document.querySelector("#textS6-3").classList.add("showTextoS6");
+  }
+  if (window.scrollY > 4950) {
+    clean();
+    document.querySelector("#imgS6-4").classList.add("showImgS6");
+    document.querySelector("#textS6-4").classList.add("showTextoS6");
+  }
+});
